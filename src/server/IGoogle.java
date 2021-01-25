2aacec0a507b16ddcfa6dadac4a64253705d13e4
EduRodriguez98 <55583447+EduRodriguez98@users.noreply.github.com>
2021-01-25 12:30:23 +0100

    deleted old version

--- src/server/IGoogle.java
+++
@@ -1,8 +0,0 @@
-package server;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IGoogle extends Remote {
-	public boolean validateUser(String username, String password) throws RemoteException;
-	public boolean registerUser(String username, String password) throws RemoteException;
-}
